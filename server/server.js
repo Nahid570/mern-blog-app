1fc3a1ebb07fd9ef1afef89ef7aa0351a6086ec1
Nahid570 <farajinahid2@gmail.com>
2022-07-24 02:07:24 +0600

    lock file

--- server/server.js
+++ server/server.js
@@ -34,6 +34,6 @@ app.use("/api/category", categoryRouter);
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 7000, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
