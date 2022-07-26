ffe7873a601ef0ae261e51316c57483d2f9a0cf9
Nahid570 <farajinahid2@gmail.com>
2022-07-26 14:45:21 +0600

    sign token updated

--- server/config/token/generateToken.js
+++ server/config/token/generateToken.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_PRIVATE_KEY, { expiresIn: "20d" });
+  return jwt.sign({ id }, `${process.env.JWT_PRIVATE_KEY}`, { expiresIn: "20d" });
 };
 
 module.exports = generateToken;
